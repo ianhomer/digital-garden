efa1a18d97b538fffc631090ad2911bde3055376
Ian Homer <code@ianhomer.com>
2021-12-20 08:19:06 +0100

    Add tailwind CSS

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="justify-center">
+      <main>
+        <h1 className="text-3xl font-bold underline">
+          Digital Garden bootstrap.
+        </h1>
+        <p className="mt-4 text-xl">Content</p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
