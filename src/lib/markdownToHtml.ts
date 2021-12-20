d858ae8aafe84868084a0931c9c7ff2edd20b149
Ian Homer <code@ianhomer.com>
2021-12-20 20:44:43 +0100

    Set up types on link shortener

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,5 +1,28 @@
-import { micromark } from "micromark";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+import { Data, Node } from "unist";
+import { visit } from "unist-util-visit";
+
+function shortenLocalLinks() {
+  return (tree: Node<Data>) => {
+    visit(tree, (node: { type: string; url: string }) => {
+      if (node.type === "link") {
+        if (node.url.startsWith("./")) {
+          node.url = "./" + /[^/]*$/.exec(node.url)[0];
+        }
+      }
+    });
+  };
+}
 
 export default async function markdownToHtml(markdown: string) {
-  return micromark(markdown);
+  const vfile = await unified()
+    .use(remarkParse)
+    .use(shortenLocalLinks)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(markdown);
+  return String(vfile);
 }
