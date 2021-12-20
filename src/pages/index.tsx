9d21c967e44d977b75c524e013c0dd85c1a4b928
Ian Homer <ian.homer@gmail.com>
2021-12-20 08:19:06 +0100

    Add tailwind CSS

--- src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div>Digital Garden bootstrap.</div>
-}
-
-export default HomePage
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/**/*.tsx",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
