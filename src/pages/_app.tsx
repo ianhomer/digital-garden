de9f1654c8ee938436e5cd79c07b5bef61b7862c
Ian Homer <code@ianhomer.com>
2021-12-21 19:47:23 +0000

    Support wiki links (#3)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container">
+    <div className="container max-w-4xl mx-2">
       <Component {...pageProps} />
     </div>
   );
