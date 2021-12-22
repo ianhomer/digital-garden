047ce1d90e9e6450b24021db61d29f4adc7f9cd5
Ian Homer <ian.homer@gmail.com>
2021-12-22 21:44:26 +0100

    Set up prebuild step

--- src/scripts/pre-build.js
+++ src/scripts/pre-build.js
@@ -2,22 +2,20 @@ const config = require("../../garden.config.js");
 const { exec } = require("child_process");
 const fs = require('fs');
 
-console.log(config.gardens);
 const cmdCallback = (error, stdout, stderr) => {
   error && console.error(`exec error: ${error}`);
   console.log(stdout);
-  stderr && console.error(`stderr: ${stderr}`);
+  console.error(stderr);
 };
 
 const gardens = config.gardens
 Object.keys(gardens).forEach((garden) => {
   const gardenDirectory = `gardens/${garden}`
   if (fs.existsSync(gardenDirectory)) {
-    console.log(`Cloning ${garden}`)
-    exec(`echo ${value}`, cmdCallback);
-  } else {
     console.log(`Garden ${gardenDirectory} already exists`)
+  } else {
+    const url = gardens[garden];
+    console.log(`Cloning ${garden}`)
+    exec(`git clone ${url} ${gardenDirectory}`, cmdCallback);
   }
 })
-console.log("Pre-build ...");
-console.log("test");
