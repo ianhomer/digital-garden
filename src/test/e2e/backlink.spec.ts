bb90986674f2856779c5ff6255bd7d2ec6604cf6
Ian Homer <ian.homer@gmail.com>
2021-12-28 08:02:29 +0100

    Lint

--- src/test/e2e/backlink.spec.ts
+++ src/test/e2e/backlink.spec.ts
@@ -1,4 +1,4 @@
-import { expect,test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("should navigate to explicit link", async ({ page }) => {
   await page.goto("/word-1");
