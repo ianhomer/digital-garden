5f52506fecc5bd749f61f2426c9e3769c232eb02
Ian Homer <code@ianhomer.com>
2022-01-06 23:56:01 +0000

    feat: code style with highlight.js (#12)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
+import "highlight.js/styles/default.css";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container max-w-4xl mx-4">
+    <div className="container max-w-4xl px-4">
       <Component {...pageProps} />
     </div>
   );
