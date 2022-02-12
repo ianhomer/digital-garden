ff33d927d3711c160d23c7597b8f6de9a82ac725
Ian Homer <ian.homer@gmail.com>
2022-02-12 00:15:07 +0000

    feat: directory child as implicit backlinks

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -18,7 +18,6 @@ export class Item {
 
   constructor(filename: string, load = false) {
     this.filename = filename;
-    console.log(filename);
     this.name = /([^/]*).md$/.exec(filename)[1];
     if (load) {
       const path = join(gardensDirectory, `${filename}`);
@@ -32,10 +31,8 @@ export class Item {
 const splitLines = (s: string) => s.split(/\n/);
 
 export async function findImplicitBackLinks(name: string): Promise<string[]> {
-  return (await findFilesInNamedDirectory(gardensDirectory, name))
-    .filter((s) => {
-      s.endsWith(".md");
-    })
+  return (await findFilesInNamedDirectory(gardensDirectory, name, 3))
+    .filter((s) => s.endsWith(".md"))
     .map((backlink: string) => /([^/]*).md$/.exec(backlink)[1]);
 }
 
