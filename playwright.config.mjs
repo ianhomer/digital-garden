75738536766a99e43d9689b89f2d16c8f498b582
Ian Homer <ian.homer@gmail.com>
2022-02-18 23:36:18 +0000

    lint

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -1,6 +1,6 @@
 import { devices } from "@playwright/test";
 import path from "path";
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
