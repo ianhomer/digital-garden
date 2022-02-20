b1985e61540916e63129c1eaeceb2ae62e5b7e12
Ian Homer <ian.homer@gmail.com>
2022-02-20 07:59:45 +0000

    add groovy language

--- packages/site/src/lib/markdownToHtml.ts
+++ packages/site/src/lib/markdownToHtml.ts
@@ -1,4 +1,5 @@
 import langGherkin from "highlight.js/lib/languages/gherkin";
+import langGroovy from "highlight.js/lib/languages/groovy";
 import rehypeHighlight from "rehype-highlight";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
@@ -11,6 +12,7 @@ import { visit } from "unist-util-visit";
 
 const languages = {
   gherkin: langGherkin,
+  groovy: langGroovy,
 };
 
 function shortenLocalLinks() {
---
+++ packages/site/src/test/content/garden1/language/languages.md
@@ -0,0 +1,25 @@
+# Languages
+
+## JSON
+
+```json
+{}
+```
+
+## XML
+
+```xml
+<root/>
+```
+
+## SQL
+
+```SQL
+SELECT * FROM people
+```
+
+### Groovy
+
+```groovy
+def foo = "value"
+```
