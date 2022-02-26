e67ed7da64f6e5887bde3dc1ebc05130b2232fc8
Ian Homer <ian.homer@gmail.com>
2022-02-26 15:41:18 +0000

    correct link name

--- packages/site/src/lib/garden/markdown.test.ts
+++ packages/site/src/lib/garden/markdown.test.ts
@@ -16,12 +16,12 @@ it("should process OK", () => {
   const meta = process(() => "# My Name\n\n[[my-link]]");
 
   expect(meta.title).toBe("My Name");
-  expect(meta.links[0].to).toBe("my-link");
+  expect(meta.links[0].name).toBe("my-link");
 });
 
 it("file thing should process OK", () => {
   const meta = process(garden.thing("garden1/word/word-1.md").content);
 
   expect(meta.title).toBe("Word 1");
-  expect(meta.links[0].to).toBe("word-2");
+  expect(meta.links[0].name).toBe("word-2");
 });
