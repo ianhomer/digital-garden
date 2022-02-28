353f9b36a71f67a8971a2d664890f61cb4f4df14
Ian Homer <code@ianhomer.com>
2022-02-28 23:42:52 +0000

    feat: unfilled circle for wanted (#43)

--- packages/site/src/lib/graph/graph.ts
+++ packages/site/src/lib/graph/graph.ts
@@ -1,30 +1,7 @@
-import { Item, ItemLink, Link, LinkType } from "../garden/types";
+import { ItemLink, Things } from "../garden/types";
 import { Graph, NodeType } from "./types";
 
-export const createDirectGraph = (item: Item, links: Array<Link>): Graph => {
-  return {
-    nodes: [
-      {
-        id: item.name,
-        type: NodeType.Thing,
-        depth: 1,
-      },
-      ...links.map((link) => ({
-        id: link.name,
-        type: NodeType.Thing,
-        depth: 1,
-      })),
-    ],
-    links: links.map((link) => ({
-      type: LinkType.To,
-      target: item.name,
-      source: link.name,
-      depth: 1,
-    })),
-  };
-};
-
-export const createGraph = (links: Array<ItemLink>): Graph => {
+export const createGraph = (things: Things, links: Array<ItemLink>): Graph => {
   const names = links
     .map((link) => [link.source, link.target])
     .flat()
@@ -48,6 +25,7 @@ export const createGraph = (links: Array<ItemLink>): Graph => {
         return {
           id: name || "na",
           type: NodeType.Thing,
+          wanted: !(name in things),
           depth,
           ...fixedCoordinates,
         };
