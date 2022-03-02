bdd779417b8c0908d968482d1c9eb53042ecbd31
Ian Homer <ian.homer@gmail.com>
2022-03-02 21:10:19 +0000

    feat: make easier for others to try and deploy (#44)

--- packages/site/garden.config.js
+++ packages/site/garden.config.js
@@ -1,17 +1,40 @@
 import dotenv from "dotenv";
+import fs from "fs";
 import { join } from "path";
-dotenv.config();
+dotenv.config({ path: "../../.env" });
+
+const gardenDirectoryFromEnv = process.env.GARDENS_DIRECTORY;
 
 function resolveDirectory() {
-  if (process.env.GARDENS_DIRECTORY) {
-    return join(process.cwd(), process.env.GARDENS_DIRECTORY);
+  if (gardenDirectoryFromEnv) {
+    return join(process.cwd(), gardenDirectoryFromEnv);
   }
-  return join(process.cwd(), "gardens");
+  return join(process.cwd(), ".gardens");
 }
 
-export default {
-  gardens: {
-    boxofjam: "https://github.com/purplepip/boxofjam.git",
-  },
-  directory: resolveDirectory(),
-};
+function gardensFromEnv() {
+  return Object.keys(process.env)
+    .filter((key) => key.startsWith("GARDEN_"))
+    .reduce((map, key) => {
+      map[key.substring(7).toLowerCase()] = process.env[key];
+      return map;
+    }, {});
+}
+
+function generateDefault() {
+  const gardens = gardensFromEnv();
+  const directory = (() => {
+    if (gardenDirectoryFromEnv || Object.keys(gardens).length) {
+      return resolveDirectory();
+    }
+    // this is the zero config, clone and run config
+    return join(process.cwd(), "src/test/content");
+  })();
+  return {
+    gardens,
+    hasMultiple: !fs.existsSync(`${directory}/README.md`),
+    directory,
+  };
+}
+
+export default generateDefault();
