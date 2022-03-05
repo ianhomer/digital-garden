e81232f12bcf3562a70d82be118c6e3fe5bf5e63
Ian Homer <ian.homer@gmail.com>
2022-03-05 22:29:15 +0000

    Experiment with collide

--- packages/site/src/components/collideRectangle.ts
+++ packages/site/src/components/collideRectangle.ts
@@ -1,10 +1,36 @@
 import { SimulationNodeDatum } from "d3-force";
+import { quadtree } from "d3-quadtree";
+
+function x(d: SimulationNodeDatum) {
+  return d.x + d.vx;
+}
+
+function y(d: SimulationNodeDatum) {
+  return d.y + d.vy;
+}
+
+function apply(d: SimulationNodeDatum) {
+  return (quad: { data: SimulationNodeDatum }) => {
+    if (quad.data && quad.data != d) {
+      const x = d.x - quad.data.x;
+      const y = d.y - quad.data.y;
+      const absX = Math.abs(x);
+      const absY = Math.abs(y);
+
+      if (absX < 10 && absY < 60) {
+        console.log("collide");
+      }
+    }
+  };
+}
 
 export default function () {
   let nodes: SimulationNodeDatum[];
   function force() {
+    const tree = quadtree(nodes, x, y);
+
     for (const d of nodes) {
-      console.log(d);
+      tree.visit(apply(d));
     }
   }
   force.initialize = (_: SimulationNodeDatum[]) => (nodes = _);
