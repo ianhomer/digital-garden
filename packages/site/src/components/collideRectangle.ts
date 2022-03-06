e882c6927047ad64e314cd4294c211a738801b64
Ian Homer <ian.homer@gmail.com>
2022-03-06 08:20:13 +0000

    Add a guide box

--- packages/site/src/components/collideRectangle.ts
+++ packages/site/src/components/collideRectangle.ts
@@ -9,7 +9,8 @@ function y(d: SimulationNodeDatum) {
   return d.y + d.vy;
 }
 
-function apply(d: SimulationNodeDatum) {
+// box is [x,y,width,height]
+function apply(d: SimulationNodeDatum, box: number[]) {
   return (quad: { data: SimulationNodeDatum }) => {
     if (quad.data && quad.data != d) {
       const x = d.x - quad.data.x;
@@ -30,13 +31,13 @@ function apply(d: SimulationNodeDatum) {
   };
 }
 
-export default function () {
+export default function (box: number[]) {
   let nodes: SimulationNodeDatum[];
   function force() {
     const tree = quadtree(nodes, x, y);
 
     for (const d of nodes) {
-      tree.visit(apply(d));
+      tree.visit(apply(d, box));
     }
   }
   force.initialize = (_: SimulationNodeDatum[]) => (nodes = _);
