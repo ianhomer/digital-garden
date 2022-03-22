3c277280997bfb0c2feed9be08282d00fe6a1824
Ian Homer <ian.homer@gmail.com>
2022-03-22 19:32:12 +0000

    Local garden CLI

---
+++ packages/site/bin/garden.cjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+// Spin up local garden
+
+console.log("Local Garden");
+
+const gardensDirectory = process.cwd();
+
+const path = require("path");
+const siteRoot = path.normalize(path.dirname(__filename) + "/..");
+
+const subprocess = require("child_process").spawn("pnpm", ["dev"], {
+  cwd: siteRoot,
+  env: {
+    ...process.env,
+    GARDENS_DIRECTORY: gardensDirectory,
+  },
+  detached: false,
+  stdio: "inherit",
+});
+
+subprocess.on("error", (err) => {
+  console.error(`Cannot start garden : ${err}`);
+});
