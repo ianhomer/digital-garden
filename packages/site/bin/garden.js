399c567bd537b67998a6bf99ff95eadf68044163
Ian Homer <ian.homer@gmail.com>
2022-03-22 08:38:38 +0000

    experiment with calling dev CLI

--- packages/site/bin/garden.js
+++ packages/site/bin/garden.js
@@ -6,6 +6,9 @@ console.log("Local Garden");
 
 const gardensDirectory = process.cwd();
 console.log(gardensDirectory);
+import { nextDev } from "next/dev.js";
+console.log(nextDev);
+nextDev();
 
 // _DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 // INSTALL_DIR="$_DIR/.."
---
+++ packages/site/patch/dev.d.ts
@@ -0,0 +1,3 @@
+import dev from "./dist/cli/next-dev";
+export * from "./dist/cli/next-dev";
+export default dev;
