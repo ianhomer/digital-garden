2d0350b00e25205d94bd8f24793633e8c600884b
Ian Homer <ian.homer@gmail.com>
2022-04-03 21:29:27 +0100

    fix: remove logs

--- packages/site/src/lib/remark-garden-directives.ts
+++ packages/site/src/lib/remark-garden-directives.ts
@@ -5,16 +5,13 @@ import { visit } from "unist-util-visit";
 export function remarkGardenDirectives() {
   return (tree: Root) => {
     visit(tree, (node) => {
-      console.log(node.type);
       if (node.type === "containerDirective") {
-        console.log(node);
         const data = node.data || (node.data = {});
         const tagName = "div";
         data.hName = tagName;
         data.hProperties = h(tagName, {
           class: "container-directive container-directive-info",
         }).properties;
-        console.log(data);
       }
     });
   };
