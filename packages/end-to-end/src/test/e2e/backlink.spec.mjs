b0ceee9b9c253fd9bf38a05d2e137c46f4036322
Ian Homer <ian@purplepip.com>
2022-06-26 10:07:16 +0100

    feat: slash at ends (#86)

--- packages/end-to-end/src/test/e2e/backlink.spec.mjs
+++ packages/end-to-end/src/test/e2e/backlink.spec.mjs
@@ -3,13 +3,13 @@ import { expect, test } from "@playwright/test";
 test("should navigate to explicit backlink", async ({ page }) => {
   await page.goto("/word-2");
   await page.click("text=word-1");
-  await expect(page).toHaveURL("/word-1");
+  await expect(page).toHaveURL("/word-1/");
   await expect(page.locator("h1")).toContainText("Word 1");
 });
 
 test("should navigate to implicit link", async ({ page }) => {
   await page.goto("/word");
   await page.click("text=word-1");
-  await expect(page).toHaveURL("/word-1");
+  await expect(page).toHaveURL("/word-1/");
   await expect(page.locator("h1")).toContainText("Word 1");
 });
