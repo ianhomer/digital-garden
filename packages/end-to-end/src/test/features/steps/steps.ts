742a0f6df16c9567ebba239b5cde350390a99afe
Ian Homer <ian@local>
2022-06-26 10:05:00 +0100

    test: expect forward slash

--- packages/end-to-end/src/test/features/steps/steps.ts
+++ packages/end-to-end/src/test/features/steps/steps.ts
@@ -7,7 +7,7 @@ Given("I am on the {word} page", async function (name) {
 
 When("I click {word}", async function (name) {
   await this.page.click(`text=${name}`);
-  await expect(this.page).toHaveURL(`/${name}`);
+  await expect(this.page).toHaveURL(`/${name}/`);
 });
 
 Then("I see the heading {string}", async function (heading) {
