233c2075df25eee481a48a110ec042fd12dc4ab7
Ian Homer <ian@local>
2022-07-31 13:58:39 +0100

    doc: indicate that pageResolver is compatible with remark-wiki-link

--- packages/garden/src/link.ts
+++ packages/garden/src/link.ts
@@ -1,4 +1,5 @@
 export const linkResolver = (name: string) =>
   name.replace(/ /g, "-").toLowerCase();
 
+// A page resolver plugin compatible with the remark-wiki-link pageResolver
 export const pageResolver = (name: string) => [linkResolver(name)];
