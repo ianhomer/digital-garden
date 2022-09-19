df38f991ca250892a1679c1c8b0b4a8d77588773
Ian Homer <code@ianhomer.com>
2022-09-19 23:02:07 +0100

    ✨ add wanted count to wanted page

--- packages/site/src/pages/x/wanted.tsx
+++ packages/site/src/pages/x/wanted.tsx
@@ -1,17 +1,27 @@
-import { findWantedThings } from "@garden/garden";
+import { findItemOrWanted, findLinks, findWantedThings } from "@garden/garden";
 
 import { garden } from "../../components/siteGarden";
 
 type AllData = {
-  items: string[];
+  items: { name: string; count: number }[];
 };
 function Wanted({ items }: AllData) {
   return (
     <ul>
       {Array.from(new Set(items))
-        .sort()
-        .map((name) => (
-          <li key={name}>{name}</li>
+        .sort((a, b) =>
+          a.count == b.count
+            ? a.name > b.name
+              ? 1
+              : -1
+            : a.count < b.count
+            ? 1
+            : -1
+        )
+        .map((item) => (
+          <li key={item.name}>
+            {item.name} : {item.count}
+          </li>
         ))}
     </ul>
   );
@@ -19,7 +29,17 @@ function Wanted({ items }: AllData) {
 
 export async function getStaticProps() {
   const things = await garden.load();
-  const items = findWantedThings(things);
+  const items = await Promise.all(
+    findWantedThings(things).map(async (name) => {
+      const item = await findItemOrWanted(garden.config, name);
+      const links = await findLinks(garden, item);
+
+      return {
+        name,
+        count: links.length,
+      };
+    })
+  );
 
   return { props: { items } };
 }
