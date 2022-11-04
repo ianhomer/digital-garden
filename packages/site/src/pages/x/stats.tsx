635dbfdb74016dda39714edb683f472347637b2e
Ian Homer <code@ianhomer.com>
2022-11-04 10:38:52 +0000

    ✨ report on single wanted things

--- packages/site/src/pages/x/stats.tsx
+++ packages/site/src/pages/x/stats.tsx
@@ -1,10 +1,13 @@
 import { findWantedThings } from "@garden/garden";
+import { LinkType } from "@garden/types";
 
 import { garden } from "../../components/siteGarden";
 
 type Props = {
   thingsCount: number;
   wantedCount: number;
+  singleWantedCount: number;
+  singleNaturalWantedCount: number;
 };
 
 function Stats(stats: Props) {
@@ -12,6 +15,8 @@ function Stats(stats: Props) {
     <ul>
       <li>item count : {stats.thingsCount}</li>
       <li>wanted count : {stats.wantedCount}</li>
+      <li>single wanted count : {stats.singleWantedCount}</li>
+      <li>single natural wanted count : {stats.singleNaturalWantedCount}</li>
     </ul>
   );
 }
@@ -19,11 +24,29 @@ function Stats(stats: Props) {
 export async function getStaticProps() {
   const things = await garden.load();
   const wantedThings = findWantedThings(things);
+  const listOfThings = Object.values(things);
+  const singleWantedThings = wantedThings.filter(
+    (name) =>
+      listOfThings.filter((thing) =>
+        thing.links.find((link) => link.name === name)
+      ).length == 1
+  );
+  const singleNaturalWantedThings = wantedThings.filter(
+    (name) =>
+      listOfThings.filter((thing) =>
+        thing.links.find(
+          (link) => link.name === name && link.type === LinkType.NaturalTo
+        )
+      ).length == 1
+  );
+  console.log(singleNaturalWantedThings);
 
   return {
     props: {
       thingsCount: Object.keys(things).length,
-      wantedCount: Object.keys(wantedThings).length,
+      wantedCount: wantedThings.length,
+      singleWantedCount: singleWantedThings.length,
+      singleNaturalWantedCount: singleNaturalWantedThings.length,
     },
   };
 }
