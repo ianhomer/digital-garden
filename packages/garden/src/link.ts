fd926b71074f016f635a4612122b04d684b77334
Ian Homer <code@ianhomer.com>
2022-11-21 02:52:28 -0800

    ✨ allow specific non standard page names (#124)

--- packages/garden/src/link.ts
+++ packages/garden/src/link.ts
@@ -7,7 +7,10 @@ export const linkResolver = (name: string) =>
 // A page resolver plugin compatible with the remark-wiki-link pageResolver
 export const pageResolver = (name: string) => [linkResolver(name)];
 
-export const isValidPageName = (name: string) => /^[0-9a-z\\-]+$/.test(name);
+const ALLOWED_ALTERNATIVE_PAGE_NAMES = ["README", ""];
+
+export const isValidPageName = (name: string) =>
+  /^[0-9a-z\\-]+$/.test(name) || ALLOWED_ALTERNATIVE_PAGE_NAMES.includes(name);
 
 export const isNotValidPageName = (name: string) => !isValidPageName(name);
 
