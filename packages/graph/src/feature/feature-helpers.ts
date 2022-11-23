4ec3fa030d0d3b53b25491c160787833dcbe391f
Ian Homer <code@ianhomer.com>
2022-11-23 15:00:06 +0000

    ✅ feature approach for graph assertions

---
+++ packages/graph/src/feature/feature-helpers.ts
@@ -0,0 +1,54 @@
+import { ItemLink, Link, Meta, Things } from "@garden/types";
+
+class MetaFactory {
+  meta;
+  thingsFactory;
+
+  constructor(thingsFactory: ThingsFactory, meta: Meta) {
+    this.thingsFactory = thingsFactory;
+    this.meta = meta;
+  }
+
+  links(...names: string[]) {
+    for (const name of names) {
+      this.meta.links.push({ name });
+    }
+    return this;
+  }
+
+  pop() {
+    return this.thingsFactory;
+  }
+}
+
+class ThingsFactory {
+  things: Things = {};
+
+  thing(name: string) {
+    const meta = {
+      title: name,
+      links: [],
+    };
+    this.things[name] = meta;
+    return new MetaFactory(this, meta);
+  }
+
+  linked(name: string, to: string) {
+    return this.thing(name).links(to).pop();
+  }
+
+  deep(base: string, count: number) {
+    for (let i = 0; i < count; i++) {
+      this.thing(`${base}-${i}`).links(`${base}-${i + 1}`);
+    }
+    return this;
+  }
+
+  create() {
+    return this.things;
+  }
+}
+
+export const factory = () => new ThingsFactory();
+export const bySource = (source: string) => (link: ItemLink) =>
+  link.source === source;
---
+++ packages/graph/src/feature/limit-depth.test.ts
@@ -0,0 +1,19 @@
+import { findDeepLinks } from "../graph";
+import { bySource, factory } from "./feature-helpers";
+
+describe("graph depth", () => {
+  it("should be limited", async () => {
+    const things = factory().deep("word", 6).create();
+    const deepLinks = findDeepLinks(things, "word-1", 3);
+    expect(deepLinks.find(bySource("word-4"))).toBeDefined();
+    expect(deepLinks.find(bySource("word-5"))).toBeUndefined();
+    expect(deepLinks.length).toBe(5);
+    expect(deepLinks).toStrictEqual([
+      { depth: 1, source: "word-1", target: "word-2", type: "to" },
+      { depth: 1, source: "word-1", target: "word-0", type: "from" },
+      { depth: 2, source: "word-2", target: "word-3", type: "to" },
+      { depth: 3, source: "word-3", target: "word-4", type: "to" },
+      { depth: 4, source: "word-4", target: "word-5", type: "to" },
+    ]);
+  });
+});
