5a9221fd5a117d262838f4105399ffee74ab55d0
Ian Homer <code@ianhomer.com>
2022-11-27 18:12:56 +0000

    ✅ remove redundant changes

--- packages/react/babel.config.cjs
+++ packages/react/babel.config.cjs
@@ -3,9 +3,4 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     ["@babel/preset-typescript", { allExtensions: true, isTSX: true }],
   ],
-  env: {
-    test: {
-      plugins: ["@babel/plugin-transform-modules-commonjs"],
-    },
-  },
 };
