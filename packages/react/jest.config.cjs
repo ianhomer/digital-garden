8adb95f40abf0526624087991ff36f21803d50eb
Ian Homer <code@ianhomer.com>
2022-11-27 18:11:27 +0000

    ✅ set up d3-quadtree as dev dependendy

--- packages/react/jest.config.cjs
+++ packages/react/jest.config.cjs
@@ -1,9 +1,3 @@
-const transformIgnores = [
-  "d3",
-  "internap",
-  "delaunator",
-  "robust-predicates",
-].join("|");
 const config = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
@@ -11,11 +5,10 @@ const config = {
   moduleNameMapper: {
     "^d3$": "<rootDir>/node_modules/d3/dist/d3.min.js",
     "^d3-quadtree$":
-      "<rootDir>/../../node_modules/.pnpm/d3-quadtree@3.0.1/node_modules/d3-quadtree/dist/d3-quadtree.min.js",
+      "<rootDir>/node_modules/d3-quadtree/dist/d3-quadtree.min.js",
   },
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  transformIgnorePatterns: [`<rootDir>/node_modules/(?!${transformIgnores})`],
 };
 
 module.exports = config;
