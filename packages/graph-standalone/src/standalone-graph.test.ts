d65fcd5a57f15db03cd5cacaf98bdd39980eccaf
Ian Homer <code@ianhomer.com>
2022-12-10 18:29:28 +0000

    ✅ test graph package bundled OK

---
+++ packages/graph-standalone/src/standalone-graph.test.ts
@@ -0,0 +1,24 @@
+import "@testing-library/jest-dom";
+
+import { findAllByText, waitFor } from "@testing-library/dom";
+import { JSDOM } from "jsdom";
+import path from "path";
+
+describe("standalone graph", () => {
+  it("should render OK from bundled package", async () => {
+    await JSDOM.fromFile(path.join(__dirname, "index.html"), {
+      resources: "usable",
+      runScripts: "dangerously",
+    }).then(async (dom) => {
+      await waitFor(
+        async () => {
+          const fooNode = await findAllByText(dom.window.document.body, "Foo");
+          expect(fooNode).toBeDefined();
+        },
+        { timeout: 3000 }
+      ).catch((error) => {
+        console.log(error, dom.serialize());
+      });
+    });
+  });
+});
