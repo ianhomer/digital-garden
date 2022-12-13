be75c835f8987204dea1a0365a6dd9cc26fbc906
Ian Homer <code@ianhomer.com>
2022-12-13 16:04:29 +0000

    📝 notes on checksum logic

--- packages/core/src/hash.ts
+++ packages/core/src/hash.ts
@@ -1,11 +1,16 @@
+// cheap checksum
 export const hash = (source: string) => {
   let value = 0;
   if (source.length === 0) {
     return value;
   }
+  // classic checksum
   for (let i = 0; i < source.length; i++) {
+    // shift (1->32), minus current and add new character
     value = (value << 5) - value + source.charCodeAt(i);
+    // bitwise to 32 bit integer
     value |= 0;
   }
+  // redix with radix 36, i.e. characters 0-9a-z
   return Math.abs(value).toString(36);
 };
