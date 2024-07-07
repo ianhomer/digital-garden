8babf60b8db4b14aba23f23adac878621ccac865
Ian Homer <code@ianhomer.com>
2024-07-07 22:02:50 +0100

    Rename graph package to visualisation

--- packages/visualisation/src/index.ts
+++ packages/visualisation/src/index.ts
@@ -1,5 +1,3 @@
-import { builder } from "@garden/core";
-
 import collideRectangle from "./collide-rectangle";
 import defaultConfiguration from "./default-configuration";
 import itemName from "./item-name";
@@ -8,4 +6,4 @@ import { GardenSimulation, Graph, GraphConfiguration } from "./types";
 
 export type { GardenSimulation, Graph, GraphConfiguration };
 
-export { builder, collideRectangle, defaultConfiguration, itemName, render };
+export { collideRectangle, defaultConfiguration, itemName, render };
