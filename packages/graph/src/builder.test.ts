817da44c98d8be135b91d7097d8f6dbfd2cbbe91
Ian Homer <code@ianhomer.com>
2024-07-08 23:22:13 +0100

    Rename things to items

--- packages/graph/src/builder.test.ts
+++ packages/graph/src/builder.test.ts
@@ -2,22 +2,22 @@ import { builder } from "./builder";
 import { byName } from "./link";
 
 describe("factor", () => {
-  it("should create empty things", () => {
-    const things = builder().build();
-    expect(Object.keys(things)).toHaveLength(0);
+  it("should create empty items", () => {
+    const items = builder().build();
+    expect(Object.keys(items)).toHaveLength(0);
   });
 
-  it("should create a single thing", () => {
-    const things = builder().name("foo").build();
-    expect(Object.keys(things)).toHaveLength(1);
-    expect(things.foo.title).toStrictEqual("foo");
+  it("should create a single item", () => {
+    const items = builder().name("foo").build();
+    expect(Object.keys(items)).toHaveLength(1);
+    expect(items.foo.title).toStrictEqual("foo");
   });
 
-  it("should create linked things", () => {
-    const things = builder().name("foo").and().name("bar").to("foo").build();
-    expect(Object.keys(things)).toHaveLength(2);
-    expect(things.bar.links.find(byName("foo"))).toBeDefined();
-    expect(things.foo.title).toStrictEqual("foo");
+  it("should create linked items", () => {
+    const items = builder().name("foo").and().name("bar").to("foo").build();
+    expect(Object.keys(items)).toHaveLength(2);
+    expect(items.bar.links.find(byName("foo"))).toBeDefined();
+    expect(items.foo.title).toStrictEqual("foo");
   });
 
   it("should create deep links", () => {
@@ -25,8 +25,8 @@ describe("factor", () => {
     expect(Object.keys(things)).toHaveLength(6);
   });
 
-  it("should create many things", () => {
-    const things = builder().many(50).build();
-    expect(Object.keys(things)).toHaveLength(50);
+  it("should create many items", () => {
+    const items = builder().many(50).build();
+    expect(Object.keys(items)).toHaveLength(50);
   });
 });
